92341c24eb3c4806bcf42f55c529a5e03886bbed
Kavkazec <oganesmkrtchyan2004@gmail.com>
2024-08-31 00:07:01 +0300

    added offer section

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.offerSlider', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    // autoplay: {
+    //     delay: 4000,
+    //     disableOnInteraction: false,
+    // },
+    // pagination: {
+    //     el: ".gallerySlider-pagination",
+    //     clickable: true,
+    //   },
+
+
+})
